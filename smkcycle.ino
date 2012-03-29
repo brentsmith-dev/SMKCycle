f869e79d3f8560194c23baf7d09df89edf9e2c84
Brent Smith <brentsmithis@gmail.com>
2012-03-29 13:41:22 -0300

    small tweaks around serial output for unity

--- smkcycle.ino
+++ smkcycle.ino
@@ -1,7 +1,7 @@
 // OPTIONS
 const boolean IS_SNES = true;
 const long SNES_MINIMUM_SPEED = 200;
-const long SNES_PEDAL_RELEASE_DELAY = 300;
+const long SNES_PEDAL_RELEASE_DELAY = 500;
 
 // INPUTS
 const int PEDAL_IN = 2;
@@ -15,17 +15,24 @@ const int START_BUTTON = 4;
 const int LEFT_BUTTON = 12;
 const int RIGHT_BUTTON = 11;
 
+// OUTPUTS (For Unity ArduinoBikeProxy.cs)
+const String RESET_FLAG          = "^";
+const String MESSAGE_DELIMITER   = "|";
+
 // Pedal Variables
 long lastPedal = 0;
 long timeSinceLastPedal = 0;
 int lastPedalIn = 0;
 boolean hasPedalled = false;
-
+boolean _stopped = true;
 // Button Variables
 boolean aPressed = false;
 boolean startPressed = false;
 boolean rightPressed = false;
 boolean leftPressed = false;
+
+// Loop Variables
+long lastLoop = millis();
 
 void setup() {
   // SNES Button Outs
@@ -45,23 +52,33 @@ void setup() {
   // Using a digital pin as +5V
   pinMode(13, OUTPUT);
   digitalWrite(13,HIGH);
+  if(IS_SNES) {
+    Serial.begin(9600);
+  } else {
+    Serial.begin(57600);
+    Serial.print(RESET_FLAG);
+  }
   
-  Serial.begin(9600);
 }
 
 void loop() {
-  checkRight();
-  checkLeft();
-  checkStart();
+  if ( millis() - lastLoop > 100){
+    lastLoop = millis();
+    checkRight();
+    checkLeft();
+    checkStart();
+  }
+  
   checkPedal();
+  
 }
 
 void sPrint(String message){
- if (IS_SNES){
+ if (!IS_SNES){
    String out = "";
-   out += "|";
+   out += MESSAGE_DELIMITER;
    out += message;
-   out += "|";
+   out += MESSAGE_DELIMITER;
    Serial.println(out);
  }else{
    Serial.println(message);
@@ -73,7 +90,7 @@ void checkRight(){
   // easier to fix in software for now
   if (digitalRead(RIGHT_IN) == HIGH){
     if (rightPressed){
-      sPrint(String('right:0'));
+      sPrint(String("right:0"));
       rightPressed = false;
       if (IS_SNES){
         digitalWrite(RIGHT_BUTTON, LOW);
@@ -81,7 +98,7 @@ void checkRight(){
     }
   }else{
     if (!rightPressed){
-      sPrint(String('right:1'));
+      sPrint(String("right:1"));
       rightPressed = true;
       if (IS_SNES){
         digitalWrite(RIGHT_BUTTON, HIGH);
@@ -95,7 +112,7 @@ void checkLeft(){
   // easier to fix in software for now
   if (digitalRead(LEFT_IN) == HIGH){
     if (leftPressed){
-      sPrint(String('left:0'));
+      sPrint(String("left:0"));
       leftPressed = false;
       if (IS_SNES){
         digitalWrite(LEFT_BUTTON, LOW);
@@ -103,7 +120,7 @@ void checkLeft(){
     }
   }else{
     if (!leftPressed){
-      sPrint(String('left:1'));
+      sPrint(String("left:1"));
       leftPressed = true;
       if (IS_SNES){
         digitalWrite(LEFT_BUTTON, HIGH);  
@@ -117,7 +134,7 @@ void checkStart(){
   // easier to fix in software for now
   if (digitalRead(START_IN) == HIGH){
     if (startPressed){
-      sPrint(String('start:0'));
+      sPrint(String("start:0"));
       startPressed = false;
       if (IS_SNES){
         digitalWrite(START_BUTTON, LOW);
@@ -125,7 +142,7 @@ void checkStart(){
     }
   }else{
     if (!startPressed){
-      sPrint(String('start:1'));
+      sPrint(String("start:1"));
       startPressed = true;
       if (IS_SNES){
         digitalWrite(START_BUTTON, HIGH);  
@@ -138,13 +155,17 @@ void checkPedal(){
   // check if a pedal state has changed
   if (digitalRead(PEDAL_IN) != lastPedalIn){
     lastPedalIn = digitalRead(PEDAL_IN);
-    pedalStateChange();
+    if (lastPedalIn == 0){
+      pedalStateChange();
+    }
   }
   
   // if a certain amount of time has passed we can assume
   // that the rider has stopped peddeling.
   if (hasPedalled){
-    if (millis() - lastPedal > SNES_PEDAL_RELEASE_DELAY){
+    if (!_stopped && millis() - lastPedal > SNES_PEDAL_RELEASE_DELAY){
+      _stopped = true;
+      sPrint(String("stop:1"));
       releaseA();
     }
   }
@@ -154,8 +175,8 @@ void pedalStateChange(){
   hasPedalled = true;
   timeSinceLastPedal = millis() - lastPedal;
   lastPedal = millis();
-  sPrint(String('speed:') += String(timeSinceLastPedal));
-  
+  sPrint(String("speed:") += String(timeSinceLastPedal));
+  _stopped = false;
   if (IS_SNES){
     if (timeSinceLastPedal < SNES_MINIMUM_SPEED){
       pressA();
